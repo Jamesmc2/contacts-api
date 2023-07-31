844b7dab44b5746bbd3b51239c1e2289c172737c
jimmy <jamesmc21998@gmail.com>
2023-07-31 12:33:53 -0500

    create and use a view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,6 +1,11 @@
 class ContactsController < ApplicationController
   def first_contact
-    contact = Contact.first
-    render json: { first_name: contact.first_name, last_name: contact.last_name, email: contact.email, phone_number: contact.phone_number }
+    @contact = Contact.first
+    render template: "contacts/show"
+  end
+
+  def all_contacts
+    @contacts = Contact.all
+    render template: "contacts/index"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -4,4 +4,5 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   get "/first_contact" => "contacts#first_contact"
+  get "/all_contacts" => "contacts#all_contacts"
 end
